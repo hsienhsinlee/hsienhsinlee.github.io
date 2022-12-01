769685e92ec44a0ee68759cd35967dd5d0de72da
Hsien-Hsin Lee <sean.lee@intel.com>
2022-12-01 15:48:58 -0500

    new sustainability category

--- MARS/htmlgenerator_v2/htmlgenerator.pl
+++ MARS/htmlgenerator_v2/htmlgenerator.pl
@@ -58,7 +58,7 @@ sub publicationByName {
 sub publicationByField {
 
 	my @types = ( "all", "journal", "conference", "workshop+poster", "bookchapters", "thesis" );
-	my @fields = ( "all", "ml", "uarch", "secure", "embed", "lpower", "fpga", "soc", "parallel", "gfx" );
+	my @fields = ( "all", "ml", "sustainability", "uarch", "secure", "embed", "lpower", "fpga", "soc", "parallel", "gfx" );
 
 	my @type_title = (
 				"All Papers",
@@ -72,6 +72,7 @@ sub publicationByField {
 	my @field_title = (
 				"All Technical Papers (by date)",
 				"Systems for Machine Learning",
+				"Sustainability",
 				"Conventional Processor Architecture and Compilers, Performance Modeling",
 				"Secure, Dependable and Autonomic Computing, DRM",
 				"Embedded Computing",
